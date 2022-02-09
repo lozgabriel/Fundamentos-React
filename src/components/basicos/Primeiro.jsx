5b92639131fe2da7479e3063bf324a72aa974655
Lozano <lozgabriel@gmail.com>
2022-02-09 18:24:03 -0300

    alteração no componente primeiro.jsx

--- src/components/basicos/Primeiro.jsx
+++ src/components/basicos/Primeiro.jsx
@@ -4,7 +4,7 @@ export default function Primeiro() {
     const msg = 'Seja Bem Vindo!' 
     return (
         <div>
-            <h2>Primeiro Componente</h2>
+            <h2>Primeiro Componente *</h2>
             <p>{msg}</p>
         </div>
     
